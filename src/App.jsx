72c151f9eb1f82351122f1744b88308a4731f3ee
Kenneth Cabobos <caboboskennethzxc@gmail.com>
2023-10-08 14:38:21 +0800

    Developed Landing page, Signup page, Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-import { 
-    Routes, 
-    Route
-} from 'react-router-dom'
-import LandingPage from "./pages/LandingPage"
-import LoginPage from "./pages/LoginPage"
+import Routing from "./Routing"
 
 function App() {
     return (
         <>
-            <header>
-                <Routes>
-                    <Route path='/' element={<LandingPage />}></Route>
-                    <Route path='/LoginPage' element={<LoginPage />}></Route>
-                </Routes>
+            <Routing />
 
-
-            </header>
         </>
     );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,45 @@
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import '../assets/landing.css'
+import logo from '../assets/img/logo.png'
 
 const LandingPage = () => {
+
+    const navigate = useNavigate();
+
+    const navigateToLoginPage = () => {
+        navigate('/LoginPage')
+    }
+
+    const navigateToGuestPage = () => {
+        navigate('/HomePage')
+    }
+
+    const navigateToSignupPage = () => {
+        navigate('/SignupPage')
+    }
+    
     return (
-        <div>
-            <h1>Landing Page</h1>
-            <Link to='/LoginPage'><button>To Login Page</button></Link>
+        <div className='landing-page'>
+            <div className='landing-container'>
+
+                <div className='landing-logo'>
+                    <img src= {logo} alt="logo" />
+                </div>
+
+                <div className='landing-title'>
+                    <h1><span>R</span>oute<span>W</span>ise</h1>
+                </div>
+
+                <div className='landing-btn'>
+                    <button onClick={navigateToLoginPage}>Sign in with Email</button>
+                    <button onClick={navigateToGuestPage}>Continue as Guest</button>
+                </div>
+
+                <div className='landing-txt'>
+                    <h5>No Account? <span onClick={navigateToSignupPage}>Create One</span></h5>
+                </div>
+
+            </div>
         </div>
     )
 }
