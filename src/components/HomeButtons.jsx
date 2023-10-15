c1bec610489b1339d5b486567f628528d40881a8
Kenneth Cabobos <caboboskennethzxc@gmail.com>
2023-10-15 12:34:41 +0800

    implementation of modal popup - code is already simplified

--- src/components/HomeButtons.jsx
+++ src/components/HomeButtons.jsx
@@ -8,12 +8,12 @@ import HindranceIcon from '../assets/img/hindrance-icon.png'
 const HomeButtons = (props) => {
     return (
         <>
-            {props.about ? <button className="home-btn" onClick={props.aboutClick}><img className="home-icon" src={ AboutIcon } alt=''/> about </button> : <></>}
-            {props.report ? <button className="home-btn" onClick={props.reportClick}><img className="home-icon" src={ ReportIcon } alt=''/> report </button> : <></>}
-            {props.notif ? <button className="home-btn" onClick={props.notifClick}><img className="home-icon" src={ NotifIcon } alt=''/> notification </button> : <></>}
-            {props.route ? <button className="home-btn" onClick={props.routeClick}><img className="home-icon" src={ RouteIcon } alt=''/> routes </button> : <></>}
-            {props.road ? <button className="home-btn" onClick={props.roadClick}><img className="home-icon" src={ CondIcon } alt=''/> road </button> : <></>}
-            {props.hindrance ? <button className="home-btn" onClick={props.hindranceClick}><img className="home-icon" src={ HindranceIcon } alt=''/> hindrance </button> : <></>}
+            {props.about ? <button className="home-btn" onClick={() => props.btnModalClick('about')}><img className="home-icon" src={ AboutIcon } alt=''/> about </button> : <></>}
+            {props.report ? <button className="home-btn" onClick={() => props.btnModalClick('report')}><img className="home-icon" src={ ReportIcon } alt=''/> report </button> : <></>}
+            {props.notif ? <button className="home-btn" onClick={() => props.btnModalClick('notif')}><img className="home-icon" src={ NotifIcon } alt=''/> notification </button> : <></>}
+            {props.route ? <button className="home-btn" onClick={() => props.btnModalClick('route')}><img className="home-icon" src={ RouteIcon } alt=''/> routes </button> : <></>}
+            {props.road ? <button className="home-btn" onClick={() => props.btnModalClick('road')}><img className="home-icon" src={ CondIcon } alt=''/> road </button> : <></>}
+            {props.hindrance ? <button className="home-btn" onClick={() => props.btnModalClick('hindrance')}><img className="home-icon" src={ HindranceIcon } alt=''/> hindrance </button> : <></>}
         </>
     )
 }
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -6,78 +6,22 @@ import HomeModals from '../components/HomeModals'
 
 const HomePage = () => {
 
-    const [aboutModal, setAboutModal] = useState(false)
-    const [reportModal, setReportModal] = useState(false)
-    const [notifModal, setNotifModal] = useState(false)
-    const [routeModal, setRouteModal] = useState(true)
-    const [roadModal, setRoadModal] = useState(false)
-    const [hindranceModal, setHindranceModal] = useState(false)
+    const [activeModal, setActiveModal] = useState("route");
 
-
-    const aboutClick = () => {
-        setAboutModal(true);
-        setReportModal(false);
-        setNotifModal(false);
-        setRouteModal(false);
-        setRoadModal(false);
-        setHindranceModal(false);
-    }
-
-    const reportClick = () => {
-        setAboutModal(false);
-        setReportModal(true);
-        setNotifModal(false);
-        setRouteModal(false);
-        setRoadModal(false);
-        setHindranceModal(false);
-    }
-
-    const notifClick = () => {
-        setAboutModal(false);
-        setReportModal(false);
-        setNotifModal(true);
-        setRouteModal(false);
-        setRoadModal(false);
-        setHindranceModal(false);
-    }
-
-    const routeClick = () => {
-        setAboutModal(false);
-        setReportModal(false);
-        setNotifModal(false);
-        setRouteModal(true);
-        setRoadModal(false);
-        setHindranceModal(false);
-    }
-
-    const roadClick = () => {
-        setAboutModal(false);
-        setReportModal(false);
-        setNotifModal(false);
-        setRouteModal(false);
-        setRoadModal(true);
-        setHindranceModal(false);
-    }
-
-    const hindranceClick = () => {
-        setAboutModal(false);
-        setReportModal(false);
-        setNotifModal(false);
-        setRouteModal(false);
-        setRoadModal(false);
-        setHindranceModal(true);
-    }
+    const btnModalClick = (modal) => {
+        setActiveModal(modal);
+      };
     
     return (
         <>
             <div className='home-modal'>
                 <HomeModals 
-                    aboutModal = {aboutModal}
-                    reportModal = {reportModal}
-                    notifModal = {notifModal}
-                    routeModal = {routeModal}
-                    roadModal = {roadModal}
-                    hindranceModal = {hindranceModal}
+                    aboutModal={activeModal === "about"}
+                    reportModal={activeModal === "report"}
+                    notifModal={activeModal === "notif"}
+                    routeModal={activeModal === "route"}
+                    roadModal={activeModal === "road"}
+                    hindranceModal={activeModal === "hindrance"}
                 />
             </div>
 
@@ -94,12 +38,7 @@ const HomePage = () => {
                     road = {true}
                     hindrance = {true}
 
-                    aboutClick = {e => aboutClick()}
-                    reportClick = {e => reportClick()}
-                    notifClick = {e => notifClick()}
-                    routeClick = {e => routeClick()}
-                    roadClick = {e => roadClick()}
-                    hindranceClick = {e => hindranceClick()}
+                    btnModalClick={btnModalClick}
                 />
             </div>
             <div className='home-Component'>
