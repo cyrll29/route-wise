bcc7b9b47a5c8da848e39eac89d42c193303951e
Kenneth Cabobos <caboboskennethzxc@gmail.com>
2023-10-21 14:12:49 +0800

    indent 4 to indent 2

--- src/views/modals/ReportModal.js
+++ src/views/modals/ReportModal.js
@@ -1,12 +1,12 @@
 import ModalHeader from '../../components/ModalHeader'
 
 const ReportModal = () => {
-    return (
-        <>
-            <ModalHeader />
-            <h1>Report Modal</h1>
-        </>
-    )
+  return (
+    <>
+      <ModalHeader />
+      <h1>Report Modal</h1>
+    </>
+  )
 }
 
 export default ReportModal
