07bc77ee231050c1278b7fbd5444be17a7c65a98
Menace <xavierwalet24@gmail.com>
2023-10-24 19:57:17 +0800

    Update ReportModal.js

--- src/views/modals/ReportModal.js
+++ src/views/modals/ReportModal.js
@@ -1,14 +1,55 @@
-import ModalHeader from '../../components/ModalHeader'
+import ModalHeader from "../../components/ModalHeader";
 
 const ReportModal = () => {
   return (
     <>
-      <ModalHeader 
-        title='Report'
-      />
-      <h1>Report Modal</h1>
-    </>
-  )
-}
+      <ModalHeader title="Report" />
+      <div className="report-modal-main">
+        <div className="report-modal-instruction">
+          <p>
+            Submit traffic and road reports for other users. Mark the location,
+            set proper titles, and describe the situation in the body.
+          </p>
+        </div>
 
-export default ReportModal
+        <div className="report-modal-mark">
+          <button className="report-modal-mark-button">
+            Mark the location
+          </button>
+          <input
+            className="report-modal-mark-location"
+            type="text"
+            placeholder="Location"
+            disabled
+          />
+        </div>
+
+        <div className="report-modal-title">
+          <p>Title:</p>
+          <input
+            className="report-modal-title-input"
+            type="text"
+            placeholder="Report TItle"
+            maxLength={50}
+          />
+        </div>
+        <div className="report-modal-category">
+          <div className="report-modal-category-div">Category</div>
+        </div>
+        <div className="report-modal-body">
+          <p>Body</p>
+          <input
+            className="report-modal-body-input"
+            type="text"
+            placeholder="Describe the situation"
+          />
+        </div>
+        <div className="report-modal-button">
+          <button className="report-modal-submit">SUBMIT</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ReportModal;
