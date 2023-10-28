6e9178943ecacd95b39c2a5eef1679d7967cc6a5
Menace <xavierwalet24@gmail.com>
2023-10-28 22:16:07 +0800

    Css for report modals

--- src/components/SearchComboBox.js
+++ src/components/SearchComboBox.js
@@ -1,71 +1,69 @@
-import { useState, useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
-import usePlacesAutocomplete, {
-    getGeocode,
-    getLatLng,
-} from "use-places-autocomplete";
-import {
-    Combobox,
-    ComboboxInput,
-    ComboboxPopover,
-    ComboboxList,
-    ComboboxOption,
-} from "@reach/combobox";
-import "@reach/combobox/styles.css"
+// import { useState, useMemo } from "react";
+// import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
+// import usePlacesAutocomplete, {
+//     getGeocode,
+//     getLatLng,
+// } from "use-places-autocomplete";
+// import {
+//     Combobox,
+//     ComboboxInput,
+//     ComboboxPopover,
+//     ComboboxList,
+//     ComboboxOption,
+// } from "@reach/combobox";
+// import "@reach/combobox/styles.css"
 
+// const SearchComboBox = () => {
 
-const SearchComboBox = () => {
+//   const [selected, setSelected] = useState(null);
 
-  const [selected, setSelected] = useState(null);
+//   return (
+//     <>
+//       <div className="places-container">
+//         <PlacesAutocomplete setSelected={setSelected} />
+//       </div>
+//     </>
+//   )
+// }
 
-  return (
-    <>
-      <div className="places-container">
-        <PlacesAutocomplete setSelected={setSelected} />
-      </div>
-    </>
-  )
-}
+// const PlacesAutocomplete = ({ setSelected }) => {
+//   const {
+//     ready,
+//     value,
+//     setValue,
+//     suggestions: {status, data},
+//     clearSuggestions,
+//   } = usePlacesAutocomplete();
 
-const PlacesAutocomplete = ({ setSelected }) => {
-  const {
-    ready,
-    value,
-    setValue,
-    suggestions: {status, data},
-    clearSuggestions,
-  } = usePlacesAutocomplete();
+//   const handleSelect = async (address) => {
+//     setValue(address, false)
+//     clearSuggestions();
 
-  const handleSelect = async (address) => {
-    setValue(address, false)
-    clearSuggestions();
+//     const result = await getGeocode({ address });
+//     const { lat, lng } = await getLatLng(result[0]);
+//     setSelected({ lat, lng })
+//   }
 
-    const result = await getGeocode({ address });
-    const { lat, lng } = await getLatLng(result[0]);
-    setSelected({ lat, lng })
-  }
-  
+//   return (
+//     <Combobox onSelect={handleSelect}>
+//       <ComboboxInput
+//         value={value}
+//         onChange={(e) => setValue(e.target.value)}
+//         disabled={!ready}
+//         className="combobox-input"
+//         placeholder="Search a place"
+//       />
+//       <ComboboxPopover>
+//         <ComboboxList>
+//           {status === "OK" &&
+//             data.map(({ place_id, description }) => (
+//               <ComboboxOption key={place_id} value={description}/>
+//             ))}
+//         </ComboboxList>
+//       </ComboboxPopover>
 
-  return (
-    <Combobox onSelect={handleSelect}>
-      <ComboboxInput 
-        value={value} 
-        onChange={(e) => setValue(e.target.value)} 
-        disabled={!ready}
-        className="combobox-input" 
-        placeholder="Search a place"
-      />
-      <ComboboxPopover>
-        <ComboboxList>
-          {status === "OK" &&
-            data.map(({ place_id, description }) => (
-              <ComboboxOption key={place_id} value={description}/>
-            ))}
-        </ComboboxList>
-      </ComboboxPopover>
+//     </Combobox>
+//   )
+// }
 
-    </Combobox>
-  )
-}
-
-export default SearchComboBox
+// export default SearchComboBox
--- src/views/modals/ReportModal.js
+++ src/views/modals/ReportModal.js
@@ -1,7 +1,19 @@
 import ModalHeader from "../../components/ModalHeader";
 import "../../assets/styles/modals.css";
+import Select from "react-select";
+
+const reportCategory = [
+  { value: 1, label: "Traffic" },
+  { value: 2, label: "Accident" },
+  { value: 3, label: "Road Blockage" },
+  { value: 4, label: "Flood" },
+];
 
 const ReportModal = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
       <ModalHeader title="Report" />
@@ -12,42 +24,59 @@ const ReportModal = () => {
             set proper titles, and describe the situation in the body.
           </p>
         </div>
+        <form className="report-modal-form" onSubmit={handleSubmit}>
+          <div className="report-modal-mark">
+            <button className="report-modal-mark-button">
+              Mark the Location
+            </button>
+            <input
+              className="report-modal-mark-location"
+              type="text"
+              placeholder="Location"
+              disabled
+            />
+          </div>
 
-        <div className="report-modal-mark">
-          <button className="report-modal-mark-button">
-            Mark the Location
-          </button>
-          <input
-            className="report-modal-mark-location"
-            type="text"
-            placeholder="Location"
-            disabled
-          />
-        </div>
-
-        <div className="report-modal-title">
-          <p>Title:</p>
-          <input
-            className="report-modal-title-input"
-            type="text"
-            placeholder="Report TItle"
-            maxLength={50}
-          />
-        </div>
-        <div className="report-modal-category">
-          <div className="report-modal-category-div">Category</div>
-        </div>
-        <div className="report-modal-body">
-          <p>Body</p>
-          <input
-            className="report-modal-body-input"
-            type="text"
-            placeholder="Describe the situation"
-          />
-        </div>
-        <div className="report-modal-button">
-          <button className="report-modal-submit">SUBMIT</button>
-        </div>
+          <div className="report-modal-title-div">
+            <div className="report-modal-title">
+              <p>Title:</p>
+            </div>
+            <div className="report-modal-title-input-div">
+              <input
+                className="report-modal-title-input"
+                type="text"
+                placeholder="Report TItle"
+                maxLength={50}
+              />
+            </div>
+          </div>
+          <div className="report-modal-category-div">
+            <button className="report-modal-category" disabled>
+              Category
+            </button>
+            <Select
+              className="report-modal-select"
+              options={reportCategory}
+              isSearchable={true}
+              placeholder="Select a category"
+            />
+          </div>
+          <div className="report-modal-body-div">
+            <div className="report-modal-body">
+              <p>Body:</p>
+            </div>
+            <div className="report-modal-body-input-div">
+              <textarea
+                className="report-modal-body-input"
+                type="text"
+                placeholder="Describe the situation"
+              ></textarea>
+            </div>
+          </div>
+          <div className="report-modal-button">
+            <button className="report-modal-submit">SUBMIT</button>
+          </div>
+        </form>
       </div>
     </>
   );
