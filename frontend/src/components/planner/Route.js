e7cb9072bd44e15c810d190269ad3fd871082d27
Menace <xavierwalet24@gmail.com>
2024-01-16 20:03:56 +0800

    Route Design

--- frontend/src/components/planner/Route.js
+++ frontend/src/components/planner/Route.js
@@ -1,31 +1,36 @@
-import { useState } from 'react'
-import RouteDetail from './RouteDetail'
+import { useState } from "react";
+import RouteDetail from "./RouteDetail";
+import "../../assets/styles/routelist.css";
 
-const Route = ({route}) => {
+import etalogo from "../../assets/img/etalogo.png";
 
-  const [showDetails, setShowDetails] = useState(null)
+const Route = ({ route }) => {
+  const [showDetails, setShowDetails] = useState(null);
 
   const handleShowDetails = () => {
-    setShowDetails(!showDetails)
-  }
+    setShowDetails(!showDetails);
+  };
 
   return (
     <>
-      {!route.departure_time ?
+      {!route.departure_time ? (
         <></>
-        : 
-        <div className='routelist-section'>
+      ) : (
+        <div className="routelist-section">
           <div onClick={handleShowDetails}>
-            <p>{route.departure_time.text}</p>
-            <p>{route.arrival_time.text}</p>
-            <p>{route.distance.text}</p>
+            <img className="route-logo-eta" src={etalogo} alt="etalogo" />
+            <p className="route-ETA">ETA</p>
+            <p className="route-arrival">{route.arrival_time.text}</p>
+            <p className="route-arrival-details">
+              {route.distance.text} | {route.duration.text}
+            </p>
           </div>
-  
+
           {showDetails ? (
             <div>
               <ul>
                 {route.steps.map((instruction, index) => (
-                  <RouteDetail key={index} instruction={instruction}/>
+                  <RouteDetail key={index} instruction={instruction} />
                 ))}
               </ul>
             </div>
@@ -33,9 +38,9 @@ const Route = ({route}) => {
             <></>
           )}
         </div>
-      }
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Route
+export default Route;
--- frontend/src/components/planner/RouteDetail.js
+++ frontend/src/components/planner/RouteDetail.js
@@ -1,40 +1,44 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const RouteDetail = ({instruction}) => {
-
-  const [showMoreDetails, setShowMoreDetails] = useState(null)
+const RouteDetail = ({ instruction }) => {
+  const [showMoreDetails, setShowMoreDetails] = useState(null);
 
   const removeBoldTags = (htmlString) => {
-    return htmlString.replace(/<\/?b>/g, '');
+    return htmlString.replace(/<\/?b>/g, "");
   };
 
   return (
-    <div className='routelist-details-section'>
-      <div onClick={() => {
-        setShowMoreDetails(!showMoreDetails)
-        console.log(instruction.steps)
-      }}
-    >
+    <div className="routelist-details-section">
+      <div
+        onClick={() => {
+          setShowMoreDetails(!showMoreDetails);
+          console.log(instruction.steps);
+        }}
+      >
         <p>{instruction.html_instructions}</p>
-        <p>{instruction.distance.text}</p>
-        <p>{instruction.duration.text}</p>
+        <p>Distance: {instruction.distance.text}</p>
+        <p>Duration: {instruction.duration.text}</p>
       </div>
-      
+
       {showMoreDetails && instruction.steps ? (
         <ul>
           {instruction.steps.map((step, stepIndex) => (
             <li key={stepIndex}>
-              <p dangerouslySetInnerHTML={{ __html: removeBoldTags(step.html_instructions) }}></p>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: removeBoldTags(step.html_instructions),
+                }}
+              ></p>
               <p>{step.distance.text}</p>
               <p>{step.duration.text}</p>
             </li>
           ))}
         </ul>
-        ) : (
+      ) : (
         <></>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default RouteDetail
+export default RouteDetail;
